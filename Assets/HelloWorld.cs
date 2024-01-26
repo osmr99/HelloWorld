d4e0b0f1ee821a2102c8da5c5e936eae2f48c567
osmr99 <omarsantiagomorales@gmail.com>
2024-01-26 11:36:23 -0600

    Class of 1/26 edits

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
+    [SerializeField] int number = 234;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        switch(number)
+        {
+            case 1:
+                Debug.Log("1");
+                Debug.Log("1");
+                Debug.Log("1");
+                Debug.Log("1");
+                Debug.Log("1");
+                Debug.Log("1");
+                break;
+            case 2:
+            case 3:
+                Debug.Log("2 or 3");
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
