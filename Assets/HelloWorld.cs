4c4e872a04580a3ad93c5f8dc654f63a88faf707
osmr99 <omarsantiagomorales@gmail.com>
2024-02-02 11:47:01 -0600

    2/2 class notes

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -5,30 +5,53 @@ using UnityEngine;
 
 public class HelloWorld : MonoBehaviour
 {
-    [SerializeField] int number = 234;
     // Start is called before the first frame update
+
+
     void Start()
     {
-        switch(number)
-        {
-            case 1:
-                Debug.Log("1");
-                Debug.Log("1");
-                Debug.Log("1");
-                Debug.Log("1");
-                Debug.Log("1");
-                Debug.Log("1");
-                break;
-            case 2:
-            case 3:
-                Debug.Log("2 or 3");
-                break;
-        }
+        Debug.Log(GetRandomName());
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    [ContextMenu("Add")]
+    void AddNumbers()
+    {
+        Debug.Log(Add(5, 3));
+    }
+
+    [ContextMenu("Print Name")]
+    void Print()
+    {
+        Debug.Log(GetRandomName());
+    }
+
+    string GetRandomName()
+    {
+        int i = UnityEngine.Random.Range(1, 3);
+        string name = "";
+        switch (i)
+        {
+            case 1:
+                name = "tom";
+                break;
+            case 2:
+                name = "steve";
+                break;
+            default:
+                name = "world";
+                break;
+        }
+        return name;
+    }
+
+    int Add(int a1, int a2)
+    {
+        return int.Parse("5");
     }
 }
